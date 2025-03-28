c10443a2b1816e3b9ffef91f615cf72be0c9dcc0
Adria <adria.jorda.m@gmail.com>
2025-03-28 10:47:37 +0100

    [add] computacion usando GPU

--- 03-05/Lab_pruebas.py
+++ 03-05/Lab_pruebas.py
@@ -1,6 +1,7 @@
 import torch
 import numpy as np
 import matplotlib.pyplot as plt
+from fontTools.unicodedata import block
 from sklearn.datasets import make_classification, make_regression
 from sklearn.model_selection import train_test_split
 import random
@@ -18,43 +19,3 @@ b = torch.zeros(size=(output_dim,),requires_grad=True)
 train_x = np.linspace(0, 3, 120)
 train_labels = 2 * train_x + 0.9 + np.random.randn(*train_x.shape) * 0.5
 
-def f(x):
-  return torch.matmul(x,w) + b
-
-def compute_loss(labels, predictions):
-  return torch.mean(torch.square(labels - predictions))
-
-def train_on_batch(x, y):
-  predictions = f(x)
-  loss = compute_loss(y, predictions)
-  loss.backward()
-  w.data.sub_(learning_rate * w.grad)
-  b.data.sub_(learning_rate * b.grad)
-  w.grad.zero_()
-  b.grad.zero_()
-  return loss
-
-# Shuffle the data.
-indices = np.random.permutation(len(train_x))
-features = torch.tensor(train_x[indices],dtype=torch.float32)
-labels = torch.tensor(train_labels[indices],dtype=torch.float32)
-
-batch_size = 4
-for epoch in range(10):
-  for i in range(0,len(features),batch_size):
-    loss = train_on_batch(features[i:i+batch_size].view(-1,1),labels[i:i+batch_size])
-  print('Epoch %d: last batch loss = %.4f' % (epoch, float(loss)))
-
-plt.scatter(train_x,train_labels)
-x = np.array([min(train_x),max(train_x)])
-with torch.no_grad():
-  y = w.numpy()*x+b.numpy()
-plt.plot(x,y,color='red')
-# Añadir etiquetas y leyenda
-plt.xlabel('X')
-plt.ylabel('Y')
-plt.legend()
-plt.title('Regresión Lineal')
-
-# Mostrar la gráfica
-plt.show()
--- 03-05/main.py
+++ 03-05/main.py
@@ -244,4 +244,72 @@ plt.title('Regresión Lineal')
 plt.show()
 # ----------------------------- --------------
 
-# Computations on GPU
+# Computations on GPU -----------------------------
+# Shuffle the data.
+indices = np.random.permutation(len(train_x))
+features = torch.tensor(train_x[indices],dtype=torch.float32)
+labels = torch.tensor(train_labels[indices],dtype=torch.float32)
+
+# --------------
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+print('Doing computations on '+device)
+
+### Changes here: indicate device
+w = torch.tensor([100.0],requires_grad=True,dtype=torch.float32,device=device)
+b = torch.zeros(size=(output_dim,),requires_grad=True,device=device)
+
+def f(x):
+  return torch.matmul(x,w) + b
+
+def compute_loss(labels, predictions):
+  return torch.mean(torch.square(labels - predictions))
+
+def train_on_batch(x, y):
+  predictions = f(x)
+  loss = compute_loss(y, predictions)
+  loss.backward()
+  w.data.sub_(learning_rate * w.grad)
+  b.data.sub_(learning_rate * b.grad)
+  w.grad.zero_()
+  b.grad.zero_()
+  return loss
+
+batch_size = 4
+for epoch in range(10):
+  for i in range(0,len(features),batch_size):
+    loss = train_on_batch(features[i:i+batch_size].view(-1,1),labels[i:i+batch_size])
+  print('Epoch %d: last batch loss = %.4f' % (epoch, float(loss)))
+
+np.random.seed(0) # pick the seed for reproducibility - change it to explore the effects of random variations
+
+n = 100
+X, Y = make_classification(n_samples = n, n_features=2,
+                           n_redundant=0, n_informative=2, flip_y=0.1,class_sep=1.5)
+X = X.astype(np.float32)
+Y = Y.astype(np.int32)
+
+split = [ 70*n//100, (15+70)*n//100 ]
+train_x, valid_x, test_x = np.split(X, split)
+train_labels, valid_labels, test_labels = np.split(Y, split)
+
+
+def plot_dataset(features, labels, W=None, b=None):
+  # prepare the plot
+  fig, ax = plt.subplots(1, 1)
+  ax.set_xlabel('$x_i[0]$ -- (feature 1)')
+  ax.set_ylabel('$x_i[1]$ -- (feature 2)')
+  colors = ['r' if l else 'b' for l in labels]
+  ax.scatter(features[:, 0], features[:, 1], marker='o', c=colors, s=100, alpha=0.5)
+  if W is not None:
+    min_x = min(features[:, 0])
+    max_x = max(features[:, 1])
+    min_y = min(features[:, 1]) * (1 - .1)
+    max_y = max(features[:, 1]) * (1 + .1)
+    cx = np.array([min_x, max_x], dtype=np.float32)
+    cy = (0.5 - W[0] * cx - b) / W[1]
+    ax.plot(cx, cy, 'g')
+    ax.set_ylim(min_y, max_y)
+  plt.show()
+plot_dataset(train_x, train_labels)
+#-----------------------------
