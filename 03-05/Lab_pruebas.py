2ddc11d7093425f66400c48f0ca9b66a14db028f
Adria <adria.jorda.m@gmail.com>
2025-04-02 11:21:52 +0200

    [add] Neural Networks and Optimizers

--- 03-05/Lab_pruebas.py
+++ 03-05/Lab_pruebas.py
@@ -15,3 +15,15 @@ learning_rate = 0.1
 w = torch.tensor([100.0],requires_grad=True,dtype=torch.float32)
 # This is our bias vector
 b = torch.zeros(size=(output_dim,),requires_grad=True)
+n = 100
+X, Y = make_classification(n_samples = n, n_features=2,
+                           n_redundant=0, n_informative=2, flip_y=0.1,class_sep=1.5)
+X = X.astype(np.float32)
+Y = Y.astype(np.int32)
+split = [ 70*n//100, (15+70)*n//100 ]
+train_x, valid_x, test_x = np.split(X, split)
+train_labels, valid_labels, test_labels = np.split(Y, split)
+dataset = torch.utils.data.TensorDataset(torch.tensor(train_x),torch.tensor(train_labels,dtype=torch.float32))
+dataloader = torch.utils.data.DataLoader(dataset,batch_size=16)
+#-----------------------------
+
