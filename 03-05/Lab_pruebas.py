b5469de30d299b2e5217cdfcb5d2d1efd7e9205e
Adria <adria.jorda.m@gmail.com>
2025-04-04 10:10:17 +0200

    [add] binary classification problem

---
+++ 03-05/Lab_pruebas.py
@@ -0,0 +1,10 @@
+# Keras is a part of Tensorflow 2.x framework. Let's make sure we have version 2.x.x of Tensorflow installed:
+# No funciona con python 3.13 que es el que estoy usando, hay que usar el python 3.8 o 3.9
+# cambiar el
+import tensorflow as tf
+from tensorflow import keras
+import numpy as np
+from sklearn.datasets import make_classification
+import matplotlib.pyplot as plt
+
+
